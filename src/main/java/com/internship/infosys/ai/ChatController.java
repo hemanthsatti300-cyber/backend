87bf56128c4e28ec04382ad96d1fb5ebe604ebbe
hemanthsatti300-cyber <hemanthsatti300@gmail.com>
2026-08-14 12:59:24 +0530

    Update ChatController.java

--- src/main/java/com/internship/infosys/ai/ChatController.java
+++ src/main/java/com/internship/infosys/ai/ChatController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/ai")
 @CrossOrigin(
-    origins = "https://agent-6a7decacc5a0a1ca77--bespoke-begonia-5037cf.netlify.app"
+    origins = "https://frontend-1xoh.onrender.com"
 )
 public class ChatController {
 
