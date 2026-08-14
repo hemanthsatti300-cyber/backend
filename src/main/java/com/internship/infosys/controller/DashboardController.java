a2229b82c1082f074601468efd1ef49de1bd34e1
hemanthsatti300-cyber <hemanthsatti300@gmail.com>
2026-08-14 13:03:40 +0530

    Update DashboardController.java

--- src/main/java/com/internship/infosys/controller/DashboardController.java
+++ src/main/java/com/internship/infosys/controller/DashboardController.java
@@ -10,7 +10,7 @@ import com.internship.infosys.service.DashboardService;
 
 @RestController
 @RequestMapping("/api/dashboard")
-@CrossOrigin(origins = "https://agent-6a7decacc5a0a1ca77--bespoke-begonia-5037cf.netlify.app")
+@CrossOrigin(origins = "https://frontend-1xoh.onrender.com")
 public class DashboardController {
 
     @Autowired
