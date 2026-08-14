674f19135a40504e15b9e570d55631fb86d54578
hemanthsatti300-cyber <hemanthsatti300@gmail.com>
2026-08-14 13:02:16 +0530

    Update AssetController.java

--- src/main/java/com/internship/infosys/controller/AssetController.java
+++ src/main/java/com/internship/infosys/controller/AssetController.java
@@ -14,7 +14,7 @@ import com.internship.infosys.service.AssetService;
 
 @RestController
 @RequestMapping("/api/assets")
-@CrossOrigin(origins = "https://agent-6a7decacc5a0a1ca77--bespoke-begonia-5037cf.netlify.app")
+@CrossOrigin(origins = "https://frontend-1xoh.onrender.com")
 public class AssetController {
 
     @Autowired
